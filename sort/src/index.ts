b4a523d9138d9f5ea0af30af5a297c6c45444a36
Cristian Buffa <bufface@gmail.com>
2019-07-18 21:30:56 -0300

    Just...One...More...Fix...

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,5 +1,6 @@
 import { Sorter } from "./Sorter";
-import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
+// import { CharactersCollection } from "./CharactersCollection";
 // import { NumbersCollection } from "./NumbersCollection";
 
 // const numbersCollection = new NumbersCollection([100000, 3, -5, 0]);
@@ -7,7 +8,17 @@ import { CharactersCollection } from "./CharactersCollection";
 // sorter.sort();
 // console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection("Xaayb");
-const sorter = new Sorter(charactersCollection);
+// const charactersCollection = new CharactersCollection("Xaayb");
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollection.data);
+linkedList.print();
