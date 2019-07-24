e163886579015b358457894b7c26acbab3905d6c
Cristian Buffa <bufface@gmail.com>
2019-07-24 20:17:00 -0300

    Building the Reporter

--- stats/src/Summary.ts
+++ stats/src/Summary.ts
@@ -4,10 +4,15 @@ export interface Analyzer {
   run(matches: MatchData[]): string;
 }
 
-export interface OutputTargeg {
+export interface OutputTarget {
   print(report: string): void;
 }
 
 export class Summary {
-  constructor(public analyzer: Analyzer, public outputTarge: OutputTargeg) {}
+  constructor(public analyzer: Analyzer, public outputTarget: OutputTarget) {}
+
+  buildAndPrintReport(matches: MatchData[]): void {
+    const output = this.analyzer.run(matches);
+    this.outputTarget.print(output);
+  }
 }
