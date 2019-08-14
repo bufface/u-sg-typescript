2a5b5cbd4f3c18f02c3d88c0f865247c7e05d5c6
Cristian Buffa <bufface@gmail.com>
2019-08-14 16:09:15 -0300

    Adding Model Properties

--- web/src/models/Collection.ts
+++ web/src/models/Collection.ts
@@ -4,10 +4,8 @@ import axios, { AxiosResponse } from 'axios';
 export class Collection<T, K> {
   models: T[] = [];
   events: Eventing = new Eventing();
-  
-  constructor(public rootUrl: string, public deserialize: (json: K) => T) {
 
-  }
+  constructor(public rootUrl: string, public deserialize: (json: K) => T) {}
 
   get on() {
     return this.events.on;
@@ -18,13 +16,12 @@ export class Collection<T, K> {
   }
 
   fetch(): void {
-    axios.get.(this.rootUrl)
-      .then((response: AxiosResponse) => {
-        response.data.forEach((value: K) => {
-          this.models.push(this.deserialize(value))
-        });
+    axios.get(this.rootUrl).then((response: AxiosResponse) => {
+      response.data.forEach((value: K) => {
+        this.models.push(this.deserialize(value));
+      });
 
-        this.trigger('change')
-      })
+      this.trigger('change');
+    });
   }
 }
