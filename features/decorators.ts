427fc7c7b8b00aaca0f46cf22a0737f60aae785f
Cristian Buffa <bufface@gmail.com>
2019-08-18 13:12:49 -0300

    Property Descriptors

--- features/decorators.ts
+++ features/decorators.ts
@@ -5,14 +5,14 @@ class Boat {
     return `this boats color is ${this.color}`;
   }
 
+  @logError
   pilot(): void {
+    throw new Error();
     console.log('swish');
   }
 }
 
-function testDecorator(target: any, key: string): void {
+function logError(target: any, key: string, desc: PropertyDescriptor): void {
   console.log('Target:', target);
   console.log('Key:', key);
 }
-
-testDecorator(Boat.prototype, 'pilot');
