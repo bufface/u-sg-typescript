24afa2f530e26115492c840f37ad56282f7c75f8
Cristian Buffa <bufface@gmail.com>
2019-08-19 10:50:06 -0300

    Let's Refactor

---
+++ server/src/controllers/loginController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+
+class LoginController {
+  getLogin(req: Request, res: Response) {
+    res.send(`
+      <form method="post">
+        <div>
+          <label>Email</label>
+          <input name="email" />
+        </div>
+        <div>
+          <label>Password</label>
+          <input name="password" type="password" />
+        </div>
+        <button>Submit</button>
+      </form>
+    `);
+  }
+}
